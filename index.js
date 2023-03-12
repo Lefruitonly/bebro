241e18691fbc1bb8be4c6c56980521af32c4f7e5
Lefruitonly <127672107+Lefruitonly@users.noreply.github.com>
2023-03-12 17:41:53 +0300

    1
    1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var token = ""
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const CharacterAI = require('node_characterai');
+const CharacterAI = require('node_characterai_edited');
 const characterAI = new CharacterAI();
 
 app.get('/', (req, res) => {
@@ -16,17 +16,18 @@ app.post('/', function(req, res) {
 
 (async() => {
     if (req.body.auth == ""){
-		characterAI.unauthenticate()
+		characterAI.unauthenticate();
 		token = await characterAI.authenticateAsGuest();
     }else{
-		characterAI.unauthenticate()
-		characterAI.setGuest(true)
-		characterAI.setAuthenticated(true)
-		characterAI.setToken(req.body.auth)
+		characterAI.unauthenticate();
+		characterAI.setGuest(true);
+		characterAI.setAuthenticated(true);
+		characterAI.setToken(req.body.auth);
 	}
-    const characterId = "v3lyisRb7INyd5BUdUKEKS1-MUTBom9dY9qV9-2ioTE"
+
+    const characterId = "v3lyisRb7INyd5BUdUKEKS1-MUTBom9dY9qV9-2ioTE";
     const chat = await characterAI.createOrContinueChat(characterId);
-    const response = await chat.sendAndAwaitResponse(req.body.msg, true)
+    const response = await chat.sendAndAwaitResponse(req.body.msg, true);
 
   res.send({
     'Answer': response.text,
